b3bf3dd4100e0f96f11f15010e9e1a7d2d08720e
2400031048 <sampathvankayala8@gmail.com>
2026-03-14 21:20:15 +0530

    Delete skillab9 directory

--- skillab9/src/main/java/com/klu/skillab9/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klu.skillab9.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.klu.skillab9.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
